6170d79868f9fff800cd5cfb2c42abddedb6924c
sindhu-nugroho <sindhuwijaya412@gmail.com>
2023-10-15 19:54:22 +0700

    Hraders Navbar with Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
+import Headers from "./components/Headers"
 
 export default function App() {
   return <BrowserRouter>
+    <Headers/>
     <Routes>
-      <Route path="/" element={Home}/>
-      <Route path="/sign-in" element={Signin}/>
-      <Route path="/sign-up" element={Signup}/>
-      <Route path="/about" element={About}/>
-      <Route path="/profile" element={Profile}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/sign-in" element={<Signin/>}/>
+      <Route path="/sign-up" element={<Signup/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
     </Routes>
     </BrowserRouter>
   
